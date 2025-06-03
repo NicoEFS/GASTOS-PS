42b06c1f24f9aaefbd630f4d98c40a8b8abdbf4f
NicoEFS <nvega@efsecuritizadora.cl>
2025-06-03 13:01:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -71,9 +71,7 @@ def estilo_tabla(df):
 # Página de Inicio
 if st.session_state.pagina == "Inicio":
     st.markdown("## Bienvenido al Panel de Información de EF Securitizadora.")
-    st.markdown("Selecciona una pestaña en la parte superior para comenzar a explorar información sobre los patrimonios separados. 
-    Dentro de estas secciones podrás encontrar tanto los gastos y su distribución mensual, 
-    como también las principales definiciones que involucran a los patrimonios separados.")
+    st.markdown("Selecciona una pestaña en la parte superior para comenzar a explorar información sobre los patrimonios separados. Dentro de estas secciones podrás encontrar tanto los gastos y su distribución mensual, como también las principales definiciones que involucran a los patrimonios separados.")
 
 # Página de Gastos
 if st.session_state.pagina == "Gastos":
